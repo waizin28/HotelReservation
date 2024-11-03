4abeffed512ccad5b4ab9262bbfd2900ef26286c
waizin28 <waizinlinn.08@gmail.com>
2024-11-03 15:58:22 -0600

    added form validation logic

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
-  styleUrls: ['./reservation-form.component.css']
+  styleUrls: ['./reservation-form.component.css'],
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
+  reservationForm: FormGroup = new FormGroup({});
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.reservationForm.valid) {
+    }
+  }
 }
--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -2,16 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
-
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    ReservationFormComponent,
-    ReservationListComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ReservationFormComponent, ReservationListComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
-export class ReservationModule { }
+export class ReservationModule {}
